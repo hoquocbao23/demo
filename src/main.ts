de4ff61051f557835b68b0385fe8abdd91d5ce5d
hoquocbao23 <hoquocbao6123@gmail.com>
2025-09-28 20:34:59 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const openApiDoc = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Noma API')
+      .setDescription('The Noma API description')
+      .setVersion('1.0')
+      .addTag('noma')
+      .build(),
+  );
+  SwaggerModule.setup(
+    'api',
+    app,
+    openApiDoc,
+  );
+  app.enableCors();
 }
+
 bootstrap();
